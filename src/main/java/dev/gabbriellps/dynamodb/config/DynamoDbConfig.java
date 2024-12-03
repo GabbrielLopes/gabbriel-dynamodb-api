053fc9b89df5eb1da4be9e764dcdef6cc596130e
José Gabriel <jhosegabriel95@gmail.com>
2024-12-03 12:39:59 -0300

    Initial commit

---
+++ src/main/java/dev/gabbriellps/dynamodb/config/DynamoDbConfig.java
@@ -0,0 +1,18 @@
+package dev.gabbriellps.dynamodb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+@Configuration
+public class DynamoDbConfig {
+
+
+    @Bean
+    public DynamoDbClient dynamoDbClient(){
+        return DynamoDbClient.builder()
+                
+                .build();
+    }
+
+}
