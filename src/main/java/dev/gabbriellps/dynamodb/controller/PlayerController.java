5259c144bfaa3ac01394bfba896675c50d5bf2a8
José Gabriel <jhosegabriel95@gmail.com>
2024-12-03 18:28:59 -0300

    feat: implementa endpoint get lista player history
    feat: implementa endpoint get lista player history

--- src/main/java/dev/gabbriellps/dynamodb/controller/PlayerController.java
+++ src/main/java/dev/gabbriellps/dynamodb/controller/PlayerController.java
@@ -1,9 +1,12 @@
 package dev.gabbriellps.dynamodb.controller;
 
 import dev.gabbriellps.dynamodb.dto.ScoreDTO;
+import dev.gabbriellps.dynamodb.entity.PlayerHistoryEntity;
 import dev.gabbriellps.dynamodb.service.PlayerService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/players")
@@ -21,6 +24,12 @@ public class PlayerController {
 
         playerService.savePlayerHistory(username, requestDTO);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{username}/games")
+    ResponseEntity<List<PlayerHistoryEntity>> listByUsername(@PathVariable("username") String username) {
+
+        return ResponseEntity.ok(playerService.listByUsername(username));
     }
 
 }
--- src/main/java/dev/gabbriellps/dynamodb/service/PlayerService.java
+++ src/main/java/dev/gabbriellps/dynamodb/service/PlayerService.java
@@ -4,6 +4,13 @@ import dev.gabbriellps.dynamodb.dto.ScoreDTO;
 import dev.gabbriellps.dynamodb.entity.PlayerHistoryEntity;
 import io.awspring.cloud.dynamodb.DynamoDbTemplate;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.enhanced.dynamodb.Key;
+import software.amazon.awssdk.enhanced.dynamodb.model.PageIterable;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryEnhancedRequest;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PlayerService {
@@ -22,4 +29,17 @@ public class PlayerService {
 
     }
 
+    public List<PlayerHistoryEntity> listByUsername(String username) {
+        Key key = Key.builder().partitionValue(username).build();
+
+        QueryConditional conditional = QueryConditional.keyEqualTo(key);
+
+        QueryEnhancedRequest queryRequest = QueryEnhancedRequest.builder()
+                .queryConditional(conditional)
+                .build();
+
+        PageIterable<PlayerHistoryEntity> response = dynamoDbTemplate.query(queryRequest, PlayerHistoryEntity.class);
+
+        return response.items().stream().collect(Collectors.toList());
+    }
 }
