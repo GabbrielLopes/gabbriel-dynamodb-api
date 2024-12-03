af5401060fcb19ef6651f3107a39ccf28b620490
José Gabriel <jhosegabriel95@gmail.com>
2024-12-03 12:47:24 -0300

    feat: configura dynamo db client aponta endpoint localstack
    feat: configura dynamo db client aponta endpoint localstack

--- src/main/java/dev/gabbriellps/dynamodb/config/DynamoDbConfig.java
+++ src/main/java/dev/gabbriellps/dynamodb/config/DynamoDbConfig.java
@@ -2,7 +2,10 @@ package dev.gabbriellps.dynamodb.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import java.net.URI;
 
 @Configuration
 public class DynamoDbConfig {
@@ -11,7 +14,8 @@ public class DynamoDbConfig {
     @Bean
     public DynamoDbClient dynamoDbClient(){
         return DynamoDbClient.builder()
-                
+                .endpointOverride(URI.create("http://localhost:4566"))
+                .region(Region.SA_EAST_1)
                 .build();
     }
 
